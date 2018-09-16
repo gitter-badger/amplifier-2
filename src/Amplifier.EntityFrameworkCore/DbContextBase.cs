425ad933939ab10a5a6edf184fe68bc69fa3109e
FabriDamazio <fabridamazio@gmail.com>
2018-09-16 16:19:26 -0300

    Fix DbContextBase class

--- src/Amplifier.EntityFrameworkCore/DbContextBase.cs
+++ src/Amplifier.EntityFrameworkCore/DbContextBase.cs
@@ -12,73 +12,80 @@ using System.Threading.Tasks;
 
 namespace Amplifier.EntityFrameworkCore
 {
-    class DbContextBase
+    /// <summary>
+    /// Base class for all DbContext classes.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    public class DbcontextBase<TKey> : DbContext where TKey : class
     {
-        public class DbcontextBase<TKey> : DbContext where TKey : struct
+        private readonly IUserSession<TKey> _userSession;
+
+        /// <summary>
+        /// DbContextBase constructor.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="userSession"></param>
+        public DbcontextBase(DbContextOptions options, IUserSession<TKey> userSession)
         {
-            private readonly IUserSession<TKey> _userSession;
+            _userSession = userSession;
+        }
 
-            public DbcontextBase(DbContextOptions options, IUserSession<TKey> userSession)
-            {
-                _userSession = userSession;
-            }
-            /// <summary>
-            /// Override SaveChangesAsync to set TenantId and auditing properties before save changes.
-            /// </summary>
-            /// <param name="cancellationToken"></param>
-            /// <returns></returns>
-            public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
-            {
-                ChangeTracker.AutomaticTenantIdAndAuditing(_userSession);
-                return await SaveChangesAsync(cancellationToken);
-            }
+        /// <summary>
+        /// Override SaveChangesAsync to set TenantId and auditing properties before save changes.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            ChangeTracker.AutomaticTenantIdAndAuditing(_userSession);
+            return await SaveChangesAsync(cancellationToken);
+        }
 
-            /// <summary>
-            /// Add QueryFilters for auditing and soft delete.
-            /// </summary>
-            /// <param name="modelBuilder"></param>
-            /// <param name="entidades"></param>
-            protected void EnableTenantAndSoftDeleteFilters(ModelBuilder modelBuilder,
-                                                       List<IMutableEntityType> entidades)
+        /// <summary>
+        /// Add QueryFilters for auditing and soft delete.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        /// <param name="entidades"></param>
+        protected void EnableTenantAndSoftDeleteFilters(ModelBuilder modelBuilder,
+                                                    List<IMutableEntityType> entidades)
+        {
+            foreach (var entityType in entidades)
             {
-                foreach (var entityType in entidades)
+                var type = entityType.ClrType;
+
+                if (typeof(ISoftDelete).IsAssignableFrom(type))
                 {
-                    var type = entityType.ClrType;
-
-                    if (typeof(ISoftDelete).IsAssignableFrom(type))
+                    if (typeof(ITenantFilter).IsAssignableFrom(type))
                     {
-                        if (typeof(ITenantFilter).IsAssignableFrom(type))
-                        {
-                            var method = SetSoftDeleteAndTenantIdFilterMethodInfo.MakeGenericMethod(type);
-                            method.Invoke(this, new object[] { modelBuilder });
-                        }
-                        else
-                        {
-                            var method = SetSoftDeleteFilterMethodInfo.MakeGenericMethod(type);
-                            method.Invoke(this, new object[] { modelBuilder });
-                        }
+                        var method = SetSoftDeleteAndTenantIdFilterMethodInfo.MakeGenericMethod(type);
+                        method.Invoke(this, new object[] { modelBuilder });
                     }
-
+                    else
+                    {
+                        var method = SetSoftDeleteFilterMethodInfo.MakeGenericMethod(type);
+                        method.Invoke(this, new object[] { modelBuilder });
+                    }
                 }
+
             }
+        }
 
-            private static readonly MethodInfo SetSoftDeleteFilterMethodInfo = typeof(DbcontextBase<TKey>).GetMethods(BindingFlags.Public | BindingFlags.Instance)
-               .Single(t => t.IsGenericMethod && t.Name == "SetSoftDeleteFilter");
+        private static readonly MethodInfo SetSoftDeleteFilterMethodInfo = typeof(DbcontextBase<TKey>).GetMethods(BindingFlags.Public | BindingFlags.Instance)
+            .Single(t => t.IsGenericMethod && t.Name == "SetSoftDeleteFilter");
 
-            private static readonly MethodInfo SetSoftDeleteAndTenantIdFilterMethodInfo = typeof(DbcontextBase<TKey>).GetMethods(BindingFlags.Public | BindingFlags.Instance)
-                .Single(t => t.IsGenericMethod && t.Name == "SetSoftDeleteAndTenantIdFilter");
+        private static readonly MethodInfo SetSoftDeleteAndTenantIdFilterMethodInfo = typeof(DbcontextBase<TKey>).GetMethods(BindingFlags.Public | BindingFlags.Instance)
+            .Single(t => t.IsGenericMethod && t.Name == "SetSoftDeleteAndTenantIdFilter");
 
-            public void SetSoftDeleteFilter<T>(ModelBuilder builder) where T : class, ISoftDelete
-            {
-                builder.Entity<T>().HasQueryFilter(item => !EF.Property<bool>(item, "IsDeleted"));
-            }
+        private void SetSoftDeleteFilter<T>(ModelBuilder builder) where T : class, ISoftDelete
+        {
+            builder.Entity<T>().HasQueryFilter(item => !EF.Property<bool>(item, "IsDeleted"));
+        }
 
-            public void SetSoftDeleteAndTenantIdFilter<T>(ModelBuilder builder) where T : class, ISoftDelete, ITenantFilter
-            {
-                builder.Entity<T>().HasQueryFilter(
-                    item => !EF.Property<bool>(item, "IsDeleted") &&
-                            (_userSession.DisableTenantFilter || EqualityComparer<TKey>.Default.Equals(EF.Property<TKey>(item, "TenantId"), _userSession.TenantId)));
-            }
+        private void SetSoftDeleteAndTenantIdFilter<T>(ModelBuilder builder) where T : class, ISoftDelete, ITenantFilter
+        {
+            builder.Entity<T>().HasQueryFilter(
+                item => !EF.Property<bool>(item, "IsDeleted") &&
+                        (_userSession.DisableTenantFilter || EqualityComparer<TKey>.Default.Equals(EF.Property<TKey>(item, "TenantId"), _userSession.TenantId)));
         }
     }
 }
