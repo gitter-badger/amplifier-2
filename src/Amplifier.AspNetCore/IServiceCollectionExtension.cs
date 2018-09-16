3984fc6f57ab814925e85be460489742b65d7ed3
FabriDamazio <fabridamazio@gmail.com>
2018-09-16 16:00:54 -0300

    Add IServiceColletionExtension methods to integrate Amplifier to Asp.NET Core

---
+++ src/Amplifier.AspNetCore/IServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Amplifier.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Amplifier.AspNetCore
+{
+    /// <summary>
+    /// IServiceCollection extension methods.
+    /// </summary>
+    public static class IServiceCollectionExtension
+    {
+        /// <summary>
+        /// Integrates Amplifier to Asp.NET Core
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddAmplifier(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IUserSession<>), typeof(UserSession<>));
+            return services;
+        }
+    }
+}
